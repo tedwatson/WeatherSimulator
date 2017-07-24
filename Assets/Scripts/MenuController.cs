f0b7b80bceb654bcfd8dfb9f01721dec09dc8283
tedwatson123 <tedwatson123@mac.com>
2017-07-24 20:29:06 +0900

    began work on menu scene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public InputField latitudeInputField;
+    public InputField longitudeInputField;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void StartButtonClicked()
+    {
+        double latitude;
+        double longitude;
+        if (double.TryParse(latitudeInputField.text, out latitude))
+        {
+
+        }
+    }
+
+    bool isGoodLatitude(double lat)
+    {
+        return true;
+    }
+
+    bool isGoodLongitude(double lat)
+    {
+        return true;
+    }
+
+
+}
