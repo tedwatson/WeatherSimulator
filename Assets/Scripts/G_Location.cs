5035094cbbe98aa9f31e3f45846cfd1710d34205
Ted Watson <tedwatson123@mac.com>
2017-08-11 11:29:47 -0400

    Added comments to G_Location class

--- Assets/Scripts/G_Location.cs
+++ Assets/Scripts/G_Location.cs
@@ -5,22 +5,18 @@ using System.Text;
 
 namespace Assets.Scripts
 {
-    class G_Location
+    class G_Location // Object for storing the deserialized JSON text from a Google Location API call
     {
-        public G_Results[] results;
-
-        public G_Location()
-        {
-            //results = new G_Results();
-        }
+        public G_Results[] results; // Array for storing the results of the API call
     }
 
-    class G_Results
+    class G_Results // Class for storing the results of a Google Location API call
     {
-        public string formatted_address;
+        public string formatted_address; // Street address, if any, at given latitude and longitude
 
         public G_Results()
         {
+            // Initialize variable
             formatted_address = null;
         } 
     }
