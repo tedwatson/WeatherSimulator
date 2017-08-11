2a359cbc0f54617eb0c66d69d175a9cb1734e7b2
Ted Watson <tedwatson123@mac.com>
2017-08-11 11:49:04 -0400

    Added comments to PublicIP class

--- Assets/Scripts/PublicIP.cs
+++ Assets/Scripts/PublicIP.cs
@@ -5,12 +5,13 @@ using System.Text;
 
 namespace Assets.Scripts
 {
-    class PublicIP
+    class PublicIP // Class used for storing JSON text information recieved from an API call
     {
-        public string ip { get; set; }
+        public string ip { get; set; } // our computer's public ip address
 
         public PublicIP()
             {
+            // Initialize variable
             ip = null;
             }
     }
